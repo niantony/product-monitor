1a9beb5c1644bdd94cfb496879faed4b139621d8
niantony <ni96@purdue.edu>
2021-08-17 17:57:43 -0400

    Comments and proxy improvements

--- main.py
+++ main.py
@@ -6,39 +6,45 @@ headers = {
     'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 }
 
-proxies = []
+proxyList = []
 count = 0
 
+# Parsing through the proxies
 with open('proxies.txt') as f:
     for line in f:
         line = line.strip()
         proxy = line.split(':')
         proxyHTTP = proxy[0] + ':' + proxy[1]
-        proxies.append(proxyHTTP)
-proxyCount = len(proxies)
+        proxyList.append(proxyHTTP)
+proxyCount = len(proxyList)
 
 while True:
     with open('bestBuySkus.txt') as f:
+            # Parsing through the skus
             for line in f:
                 line = line
                 sku = line.strip('\n')
 
+                # Proxy Handling
                 if proxyCount != 0:
                     requestProxy = {
-                        "http": "http://" + proxies[count],
-                        "https": "http://" + proxies[count],
+                        "http": "http://" + proxyList[count],
+                        "https": "http://" + proxyList[count],
                     }
                     count += 1
                 else:
                     requestProxy = {}
-
+                
                 if count == proxyCount:
                     count = 0
 
                 url = "https://www.bestbuy.com/site/" + sku + ".p?skuId=" + sku
+
+                # Request to BestBuy product page
                 response = requests.request("GET", url, headers=headers)
                 response.proxies = requestProxy
 
+                # Scraping product details
                 soup = BeautifulSoup(response.text, 'html.parser')
                 name = soup.find('h1', attrs={'class': 'heading-5 v-fw-regular'}).get_text()
                 image = soup.find('img', attrs={'class': 'primary-image'})['src']
@@ -46,6 +52,7 @@ while True:
                 addToCart = 'https://api.bestbuy.com/click/-/' + sku + '/cart'
                 availability = soup.find('button', attrs={'class': 'add-to-cart-button'}).get_text()
 
+                # Sends webhook notification if in stock
                 if (availability != 'Sold Out'):
                     sendBestBuyNotification(sku, url, name, image, price, addToCart)
                 else:
